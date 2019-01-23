9d7b9876fd91052c3ac5865be27ab3636b4c0a8f
okb <okb.for.git@gmail.com>
2019-01-23 17:07:16 +0000

    create views of index, show, new, edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(content: 'test task 1')
+Task.create(content: 'test task 2')
+Task.create(content: 'test task 3')
