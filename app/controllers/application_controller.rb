9c4b0f6d9d4f45444c7dd2f96910b4fd894ce5eb
okb <okb.for.git@gmail.com>
2019-02-03 06:09:26 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery with: :exception
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      flash[:danger] = 'ログインして下さい'
+      redirect_to login_url
+    end
+  end
 end
