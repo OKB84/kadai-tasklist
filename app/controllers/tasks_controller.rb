e76bcc742116d6e916353253120bed5591be21ce
okb <okb.for.git@gmail.com>
2019-02-04 04:57:44 +0000

    delete set_task method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
@@ -41,10 +40,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   # Strong Parameter
   def task_params
